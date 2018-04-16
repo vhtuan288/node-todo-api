67b44536e0c04dfac812d6467e6a620a5d9d83a2
Vu Huy Tuan <vh.tuan288@gmail.com>
2018-04-16 15:05:55 +0700

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const _ = require('lodash');
-var express = require('express');
-var bodyParser = require('body-parser');
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
+
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
@@ -61,6 +61,32 @@ app.delete('/todos/:id', (req, res) => {
     res.send(todo);
   }, (e) => {
     res.send(400).send(e);
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    } 
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
