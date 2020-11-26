ca49e9e0018417c3ca091eead3c75fb237bb4b01
Tuan Vu <vh.tuan288@gmail.com>
2020-11-26 14:28:27 +0700

    update connect uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb+srv://tuanvu288:Admin123@mern.fv27g.mongodb.net/todoDB?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log(err));
 
 module.exports = {mongoose}
